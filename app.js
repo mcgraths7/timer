6a196e0f815d026496356c937233e05a0b052739
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-21 14:27:51 -0400

    timer mostly complete, only need to emit events to notify border

--- app.js
+++ app.js
@@ -4,59 +4,50 @@ class Timer {
     this.startButton = startButton
     this.pauseButton = pauseButton;
     this.durationInput = durationInput;
-    
-    // Internal Properties
-    this.startTime = parseInt(durationInput.value);
-    this.currentTime = parseInt(durationInput.value);
-    this.intervalId = undefined;
-
-    // Bind context of "this" so we can use arrow functions
-    this.start = this.start.bind(this);
-    this.pause = this.pause.bind(this);
+    this.isStarted = false;
 
     // Add event listeners to bound DOM elements
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
-    this.durationInput.addEventListener('change', (e) => {
-      this.startTime = e.currentTarget.value;
-      this.currentTime = e.currentTarget.value;
-    });
-    this.durationInput.addEventListener('focus', () => {
-      this.pause();
-    });
-    this.durationInput.addEventListener('blur', () => {
-      this.start();
-    });
-  }
+    this.durationInput.addEventListener('focus', this.pause);
+    this.durationInput.addEventListener('blur', this.start);
+  };
   
-  tick() {
-    if (this.currentTime === 0) {
-      return this.pause();
+  tick = () => {
+    if (this.timeRemaining >= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
     }
-    this.currentTime -= 1;
-    const tickEvent = new CustomEvent('tick', { detail: {
-      startTime: this.startTime,
-      currentTime: this.currentTime,
-    }});
-    dispatchEvent(tickEvent);
-    console.log(this.currentTime);
+  };
+
+  // onDurationChange = (newTime) => {
+  //   this.pause();
+
+  //   this.start();
+  // };
+
+  start = () => {
+    if (!this.isStarted) {
+      this.isStarted = true;
+      this.tick();
+      this.intervalId = setInterval(this.tick, 1000);
+    };
+  };
+
+  pause = () => {
+    if (this.isStarted) {
+      this.isStarted = false;
+      clearInterval(this.intervalId);
+    };
+  };
+
+  get timeRemaining() {
+    return this.durationInput.value;
   }
 
-  onDurationChange(newTime) {
-    this.pause();
-    this.startTime = newTime;
-    this.currentTime = newTime;
-    this.start();
-  }
-
-  start() {
-    this.intervalId = setInterval(() => {
-      this.tick()
-    }, 1000);
-  }
-
-  pause() {
-    clearInterval(this.intervalId);
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
 }
 
